93019309a05d39bc3b603e91764e4d4ea47f0ca4
tainarahamilcas-gif <tainara.hamilcas@escola.pr.gov.br>
2026-09-21 08:11:04 -0300

    dgfvfdrt

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-resultados");
+const textoresultados = document.querySelector(".texto-resultados");
+
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+        alternativas: [
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    },
+    {
+         enunciado: "pergunta 1",
+        alternativas: [
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    }
+];
